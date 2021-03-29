cb037844ea446caece2467a56e8f0fb9e4780905
Ahmad Fauzan <12050111663@students.uin-suska.ac.id>
2021-03-30 00:24:06 +0700

    Sololearn

---
+++ Course/src/Sololearn/BasicConcept/Module1Quiz2.java
@@ -0,0 +1,10 @@
+package Sololearn.BasicConcept;
+
+public class Module1Quiz2 {
+    public static void main(String[] args) {
+        String name;
+        name = "Fauzan";
+
+        System.out.println(name);
+    }
+}
