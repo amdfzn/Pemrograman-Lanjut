a415824c641adabbde3a423c0d628c5e0073cc7e
Ahmad Fauzan <12050111663@students.uin-suska.ac.id>
2021-03-30 00:26:05 +0700

    Sololearn

---
+++ Course/src/Sololearn/BasicConcept/TimeConverter.java
@@ -0,0 +1,16 @@
+package Sololearn.BasicConcept;
+
+import java.util.*;
+public class TimeConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int days = scanner.nextInt();
+
+        //your code goes here
+        int hours = days * 24;
+        int minutes = hours * 60;
+        int seconds = minutes * 60;
+
+        System.out.println(seconds);
+    }
+}
