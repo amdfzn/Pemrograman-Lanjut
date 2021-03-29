fae99509143dff35d98d787042f1f94e046f1fbd
Ahmad Fauzan <12050111663@students.uin-suska.ac.id>
2021-03-30 00:20:52 +0700

    Sololearn

---
+++ Course/src/Sololearn/BasicConcept/Module1Quiz.java
@@ -0,0 +1,12 @@
+package Sololearn.BasicConcept;
+
+public class Module1Quiz {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 7;
+        int sum = x + y;
+
+        System.out.println(sum);
+    }
+
+}
---
+++ Course/src/Sololearn/BasicConcept/StringTask.java
@@ -0,0 +1,10 @@
+package Sololearn.BasicConcept;
+
+public class StringTask {
+    public static void main(String[] args) {
+        String var;
+        var = "Hello Fauzan";
+
+        System.out.println(var);
+    }
+}
