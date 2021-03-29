427d4524621b3f0ea44af4df3d8d6b9c5c515830
Ahmad Fauzan <12050111663@students.uin-suska.ac.id>
2021-03-30 00:29:32 +0700

    Sololearn

---
+++ Course/src/Sololearn/ConditionalAndLoop/DecisionMaking.java
@@ -0,0 +1,11 @@
+package Sololearn.ConditionalAndLoop;
+
+public class DecisionMaking {
+    public static void main(String[] args) {
+        int x = 5;
+
+        if ( x == 5 ) {
+            System.out.println("Yeah");
+        }
+    }
+}
