617258ac9f7cf35d30a3fc1b62fbf1437cc2455c
Ahmad Fauzan <12050111663@students.uin-suska.ac.id>
2021-03-30 10:52:07 +0700

    Sololearn

---
+++ Course/src/Sololearn/ConditionalAndLoop/IfElseStatement.java
@@ -0,0 +1,14 @@
+package Sololearn.ConditionalAndLoop;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+
+        if( x > y ) {
+            System.out.println(x);
+        } else {
+            System.out.println(y);
+        }
+    }
+}
