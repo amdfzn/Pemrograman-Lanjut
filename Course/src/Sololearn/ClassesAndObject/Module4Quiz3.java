eeaac0aa94c46960d4bfff39522bb936d183916c
Ahmad Fauzan <12050111663@students.uin-suska.ac.id>
2021-03-30 22:58:05 +0700

    SololearnJava

---
+++ Course/src/Sololearn/ClassesAndObject/Module4Quiz3.java
@@ -0,0 +1,12 @@
+package Sololearn.ClassesAndObject;
+
+public class Module4Quiz3 {
+    public static void main(String[] args) {
+        myFunc();
+    }
+
+    private static void myFunc() {
+        System.out.println("Hi");
+        }
+    }
+}
