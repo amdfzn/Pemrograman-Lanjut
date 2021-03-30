080c8f2ec41de00b62a1dd3711364350ab698587
Ahmad Fauzan <12050111663@students.uin-suska.ac.id>
2021-03-30 10:55:12 +0700

    Sololearn

---
+++ Course/src/Sololearn/ConditionalAndLoop/NestedIfStatement.java
@@ -0,0 +1,12 @@
+package Sololearn.ConditionalAndLoop;
+
+public class NestedIfStatement {
+    public static void main(String[] args) {
+        int x = 37;
+        if (x > 22) {
+            if (x > 31) {
+                System.out.println("it works!");
+            }
+        }
+    }
+}
