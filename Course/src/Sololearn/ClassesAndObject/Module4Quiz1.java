af0958e467e7a2f4e9d382212eceb9a6cca8cf26
Ahmad Fauzan <12050111663@students.uin-suska.ac.id>
2021-03-30 22:39:18 +0700

    SololearnJava

---
+++ Course/src/Sololearn/ClassesAndObject/Module4Quiz1.java
@@ -0,0 +1,10 @@
+package Sololearn.ClassesAndObject;
+
+public class Module4Quiz1 {
+    public static void main(String[] args) {
+
+    }
+    public int myFunc(int x) {
+        return x*10;
+    }
+}
---
+++ Course/src/Sololearn/ClassesAndObject/Module4Quiz2.java
@@ -0,0 +1,16 @@
+package Sololearn.ClassesAndObject;
+
+public class Module4Quiz2 {
+    public static void main(String[] args) {
+        public int minFunc ( int n1, int n2){
+
+            int min;
+            if (n1 > n2) {
+                min = n2;
+            } else {
+                min = n1;
+                return min;
+            }
+        }
+    }
+}
